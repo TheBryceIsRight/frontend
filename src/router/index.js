0fb45c9d92d3416f7e4735d380cbea4656899051
Bryce Watson <brycewatson315@gmail.com>
2022-04-08 17:55:52 -0400

    meta commentary

--- src/router/index.js
+++ src/router/index.js
@@ -10,26 +10,92 @@ const routes = [
     path: "/",
     name: "Home",
     component: HelloWorld,
+    meta: {
+      title: 'Home Page',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'The home page of my portfolio.'
+        },
+        {
+          property: 'og:description',
+          content: 'The home page of my portfolio.'
+        }
+      ]
+    }
   },
   {
     path: "/fze",
     name: "Functionize",
     component: Functionize,
+    meta: {
+      title: 'Functionize',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'The functionize page of my portfolio.'
+        },
+        {
+          property: 'og:description',
+          content: 'The functionize page of my portfolio.'
+        }
+      ]
+    },
   },
   {
     path: "/elavon",
     name: "DevPortal",
     component: DevPortal,
+    meta: {
+      title: 'Dev Portal',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'The Developer Portal page of my portfolio.'
+        },
+        {
+          property: 'og:description',
+          content: 'The Developer Portal of my portfolio.'
+        }
+      ]
+    },
   },
   {
     path: "/status",
     name: "SystemOutages",
     component: SystemOutages,
+    meta: {
+      title: 'System Outages',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'The system outages page of my portfolio.'
+        },
+        {
+          property: 'og:description',
+          content: 'The system outages page of my portfolio.'
+        }
+      ]
+    },
+    
   },
   {
     path: "/cdcproject",
     name: "CDCProject",
     component: CDCProject,
+    meta: {
+      title: 'CDC Project',
+      metaTags: [
+        {
+          name: 'description',
+          content: 'The air toxcity page of my portfolio.'
+        },
+        {
+          property: 'og:description',
+          content: 'The air toxcity page of my portfolio.'
+        }
+      ]
+    },
   },
 ];
 
