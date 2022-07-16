e589b4fcf563b78bbecd9d1ef5cc34d4dc25ceee
Bryce Watson <brycewatson315@gmail.com>
2022-07-16 15:35:45 -0400

    updated work logo

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -10,7 +10,7 @@
         class="white--text align-end"
         gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
         height="400px"
-        :alt= "project.attributes.Image.data.attributes.alternativeText"
+        :alt= "project.attributes.Alt"
         cover
         >
         <div class="profile-info">
